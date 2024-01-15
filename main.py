88fce90c319c99ec8e854e435fbdd7e7b46ab03b
Sumeet Chougule <sumeetchougule@live.com>
2024-01-15 17:40:06 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from src.wineQ import logging
 
-logging.info("Welcome to our custom logging")
+from src.wineQ import logging
+from src.wineQ.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+# from wineQ.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+# from wineQ.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+# from wineQ.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+# from wineQ.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logging.exception(e)
+    raise e
--- research/01_data_ingestion.py
+++ research/01_data_ingestion.py
@@ -1,10 +1,17 @@
+import os
+
+os.chdir("../")
+
+
 from dataclasses import dataclass
 from pathlib import Path
+from src.wineQ.constants import *
+from src.wineQ.utils.common import read_yaml, create_directories
 
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
-    source_URL: str
+    source_url: str
     local_data_file: Path
     unzip_dir: Path
